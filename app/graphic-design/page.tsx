b97ca7b2787877ecad3d168b966c8b5ab11165e8
Alessandra Krick <alessandrakrick@gmail.com>
2026-01-06 08:43:10 +0400

    Add missing images to graphic design page
    - Replace Google Sites URLs with local images (image44-49)
    - Update Kaju logo to use image44.png
    - Update Ephzen logo to use image45.png
    - Update Community logo to use image46.png
    - Update Saber Pet logo to use image47.png
    - Update SushiTime logo to use image48.png
    - Update Sushi logo concept to use image49.png
    - Remove unoptimized prop from Image components for better performance

--- app/graphic-design/page.tsx
+++ app/graphic-design/page.tsx
@@ -14,37 +14,37 @@ const GraphicDesign = (): JSX.Element => {
   const portfolioItems = [
     {
       id: 1,
-      image: "https://lh3.googleusercontent.com/sitesv/AAzXCkf6helipxXUfIol165MYaUTOxKzw5SdPmyP1zS4oY5R22SdHGerFi2yK_KLiACx3-zUetJJJxx1Iq2OdBFnuATbwK_tJCrumJkaN6s6waUgiwedRHSeM9fgaRWW6ltKrk78DeXrJ7YItZf1YajDjKsWXdpzCj9OzAfFiGJKT0wCopyXQJm7ShsOOzUO1A6kGd_CWRfp9oohkOqjPgogsaMVnBilxSUbYsq3=w1280",
+      image: "/images/image44.png",
       title: "Kaju",
       subtitle: "Natural Restaurant"
     },
     {
       id: 2,
-      image: "https://lh3.googleusercontent.com/sitesv/AAzXCkcdffzotm_qfOHE_8o5uxY0x9iZK8zPrqPMORNXDPv6pcLraiiyDMNNYp5JRb1syvbOEuGmOLfQSXJOAV7SmDENoJW7iUDqlMNVYS5dBGmOxb-AA8oDHwDL0wwy22I5JyE6pRAVCI5Rhqvkre1GvmGe80i0Sn9hH54l2nACL5nH8jpS6xxqwnTwndCSoYoYRfFF84e_i4ik_m30kLkgtN5QOMX50NSEdR5DPjI=w1280",
+      image: "/images/image45.png",
       title: "Ephzen",
       subtitle: "Well-being community (contain Greek characters on the 'eph')"
     },
     {
       id: 3,
-      image: "https://lh3.googleusercontent.com/sitesv/AAzXCkeRtqzSyGiqBzDZvdwrSNey_bEhwP1-5iG1pPg1ot6GJa49Yke5UnGRInCIqfVkmy_rSB1CcCAHQ0n48elf6-z07_xcV6ynCgCDYFBcnPvINW2tfIU8ZQAcT1eRtTOvrLX7GgxIcNRP_Q5E8hr2_y2UaLbOK30iZNGKaBBZIlj31ZZ30CiiIQGxFKswHONqrdvJ4V7YJd-uFL_Bqw8CXKIAyqckkA2EdGG7ZfI=w1280",
+      image: "/images/image46.png",
       title: "Community",
       subtitle: "Volunteering Platform"
     },
     {
       id: 4,
-      image: "https://lh3.googleusercontent.com/sitesv/AAzXCkehhBN7fvPVikOd2WTTwDbtHzm4c7UNxc7g0Iyt_av7dxXe8wWzLIWatAlN8IV0XwRUV255UaI_l5n6v9ERQns6yz0uuLuv87BfkdNMjwEDjcrrAibtDkNb-SULR1nWyX1aKRL-isyIHMZL6Ftl_erZ6tu91QAdgdrCoVIKPlFSIulllabcnsvilUgtpVBowhdyjf4sZKZYsKXkbJ_tWM2k5YaJo4Uwt0VmGqc=w1280",
+      image: "/images/image47.png",
       title: "Saber Pet",
       subtitle: "Pet Adestration"
     },
     {
       id: 5,
-      image: "https://lh3.googleusercontent.com/sitesv/AAzXCke6FGH5dTTVgZLVjYG3qSzpojmXBRfm9n5Uos2pctmdtBapVBuRC1QJrneWXRcncM07NdONO9-xmjxJF1FAvy5BASCVYlrWT-EWXSUUfRS9ZeiGnyA7AJqfSzxSitmy63gDtWA8qqiyrnM6mO9EG_CsHTdf6KM_Pnur8BsizHdM3C3STBZQdGZyPsyf4ArabksDzNov7kp8pk2ZmWvgtKBDSReD4wgIeC0QCBU=w1280",
+      image: "/images/image48.png",
       title: "SushiTime",
       subtitle: "Sushi Reservation Application"
     },
     {
       id: 6,
-      image: "https://lh3.googleusercontent.com/sitesv/AAzXCkfoBR8jA7aKlPa4c0dflypCQxPtWpBt51hh84sqyY2xbapU_eW7gUZftXJeuHNUb6QyinYlVj8pnIdhTW-E5pQg-Iiw_0RobdtMyKwo9wpBvzAXLtoR5ur-ZNMTgkBbdHSGsYiQnPBbCO0mZ-iH45WMimICDgkY0Gm5CXe2UWLQ0M19uO3qKi7lgITNQIiHFb7vymaOdbvvzjYKV58O4MdcjVCyBjJWbumDaGk=w1280",
+      image: "/images/image49.png",
       title: "Sushi logo concept",
       subtitle: ""
     }
@@ -102,7 +102,6 @@ const GraphicDesign = (): JSX.Element => {
                       width={1280}
                       height={720}
                       className="object-contain w-full h-auto transition-transform duration-300 group-hover:scale-105"
-                      unoptimized
                     />
                   </motion.div>
                 </div>
